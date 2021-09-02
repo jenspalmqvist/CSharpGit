9e7c2eed217f931f5bd858ff783334901697049b
Jens Palmqvist <jpalmqvi@gmail.com>
2021-09-02 15:46:18 +0200

    Initial change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace CSharpGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Changes!");
         }
     }
 }
