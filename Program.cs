b5b42ef80d3a33b5973cbeebcee4167d2c6414bd
Sandraeng <sandra.m.b.eng@hotmail.com>
2021-09-03 10:33:20 +0200

    green

--- Program.cs
+++ Program.cs
@@ -31,9 +31,14 @@ namespace CSharpGit
             Console.WriteLine("JEEEeeeeeens");
             Console.WriteLine("Boink!");
 
-
-
             Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+
+<<<<<<< HEAD
+=======
+            Console.WriteLine("This is my change. There are many like it, but this one is mine. Without me my change is useless. Without my change I am useless!");
+>>>>>>> 35ef631ec94aa1bf8ce0f6ab492bf914a3db5d94
+
 
 
             Console.WriteLine("äntligen!!!!!!!!!!");
@@ -48,7 +53,15 @@ namespace CSharpGit
             Console.WriteLine("Hellooo!");
             Console.WriteLine("42, is the answer to the ultimate question");
             Console.WriteLine("42");
+<<<<<<< HEAD
+
+            Console.WriteLine("Ute i skogen där växa blåbär");
+
+=======
             Console.WriteLine("Try again");
+            Console.WriteLine("hallå hallå hallå hallå hallå hallå");
+            string hello = "hallå hallå hallå hallå hallå ";
+>>>>>>> 35ef631ec94aa1bf8ce0f6ab492bf914a3db5d94
         }
     }
 }
