4ec09cf6d00ad3b6936146b5f8af7dbbfdec5add
Durim Miziraj <durim.miziraj@gmail.com>
2021-09-03 10:33:56 +0200

    .

--- Program.cs
+++ Program.cs
@@ -38,6 +38,7 @@ namespace CSharpGit
             Console.WriteLine("42, is the answer to the ultimate question");
             Console.WriteLine("42");
             Console.WriteLine("Try again");
+            Console.WriteLine("lalalalalala");
         }
     }
 }
