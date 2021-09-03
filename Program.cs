ded5c8e1c29a926f5b770e061215354c02ab1fab
didriknilsson <89788648+didriknilsson@users.noreply.github.com>
2021-09-03 10:28:29 +0200

    New line

--- Program.cs
+++ Program.cs
@@ -2,14 +2,15 @@
 
 namespace CSharpGit
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Changes are fun!");
             Console.WriteLine("Changes are cool!");
             Console.WriteLine("Making new changes!");
+            Console.WriteLine("Fortsätter förstöra med min egna branch..");
         }
     }
 }
