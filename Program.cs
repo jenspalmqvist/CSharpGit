bc2aa32fcaefeea2a8e868af8d38e93bda6003ec
Sandraeng <sandra.m.b.eng@hotmail.com>
2021-09-03 10:26:13 +0200

    blå

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace CSharpGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Awesome sauce");
             Console.WriteLine("Changes are fun!");
             Console.WriteLine("Changes are cool!");
+            Console.WriteLine("Hej hopp!!!");
+
+            Console.WriteLine("Mike is soo cööl");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("Ute i skogen där växa blåbär");
         }
     }
 }
