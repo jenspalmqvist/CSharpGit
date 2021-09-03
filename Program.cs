e935f5bc6f189ac52c86e627f7ea51ed1c60304d
didriknilsson <89788648+didriknilsson@users.noreply.github.com>
2021-09-03 10:22:13 +0200

    added new line

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 namespace CSharpGit
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Changes are fun!");
