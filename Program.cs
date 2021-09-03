7bafffbf759235bcdedbdc6afcbb5ee7a9fad33c
tive <svensamkarl@gmail.com>
2021-09-03 09:53:56 +0200

    lägg till en kubikrot

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace CSharpGit
             Console.WriteLine("Hello World!");
             Console.WriteLine("Changes are fun!");
             Console.WriteLine("Changes are cool!");
+            Console.WriteLine(Math.Cbrt(1000.0));
         }
     }
 }
