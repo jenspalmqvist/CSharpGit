5cf864ba8d92f0b19f6a386fec5825f09953b928
Sandraeng <sandra.m.b.eng@hotmail.com>
2021-09-03 10:29:42 +0200

    hej

--- Program.cs
+++ Program.cs
@@ -6,12 +6,34 @@ namespace CSharpGit
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Awesome sauce");
             Console.WriteLine("Changes are fun!");
             Console.WriteLine("Changes are cool!");
+            Console.WriteLine("Hej hopp!!!");
 
             Console.WriteLine("Mike is soo cööl");
             Console.WriteLine();
+
+
+
+            Console.WriteLine("The cow says SHAZOOO!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Changes are fun!");
+            Console.WriteLine("Changes are cool!");
+            Console.WriteLine(Math.Cbrt(1000.0));
+            Console.WriteLine("Mike is soo cööl");
+            Console.WriteLine();
+            Console.WriteLine("HELLOOO");
+            Console.WriteLine("Just living life and making changes!");
+            Console.WriteLine();
+            Console.WriteLine("JEEEeeeeeens");
+            Console.WriteLine("Boink!");
+            Console.WriteLine("New line");
+            Console.WriteLine("Vem vare som kasta!??");
+            Console.WriteLine("Hellooo!");
+            Console.WriteLine("42");
+
+            Console.WriteLine("Ute i skogen där växa blåbär");
 
         }
     }
