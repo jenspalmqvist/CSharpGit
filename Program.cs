3dee0d283cce1aea307a0b9cf58b397dabf2cf10
Johan Ljungberg <jlj.9@hotmail.com>
2021-09-03 10:26:15 +0200

    förklaring

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace CSharpGit
             Console.WriteLine("New line");
             Console.WriteLine("Vem vare som kasta!??");
             Console.WriteLine("Hellooo!");
+            Console.WriteLine("Jens kasta!");
         }
     }
 }
