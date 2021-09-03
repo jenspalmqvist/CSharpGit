18b4a606be325192e8795a1413ac15c3559524d8
didriknilsson <89788648+didriknilsson@users.noreply.github.com>
2021-09-03 10:30:36 +0200

    New line

--- Program.cs
+++ Program.cs
@@ -20,23 +20,14 @@ namespace CSharpGit
             Console.WriteLine("JEEEeeeeeens");
             Console.WriteLine("Boink!");
 
-
-
-
-
-
-
             Console.WriteLine("äntligen!!!!!!!!!!");
-
-
-
-
 
             Console.WriteLine("New line");
             Console.WriteLine("Vem vare som kasta!??");
             Console.WriteLine("Hellooo!");
             Console.WriteLine("42, is the answer to the ultimate question");
             Console.WriteLine("42");
+            Console.WriteLine("Skriver det igen. Extremt användbart program!");
         }
     }
 }
